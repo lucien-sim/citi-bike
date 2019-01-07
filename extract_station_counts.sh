3922c4d8c10f7567a1d2294e47901c818eb3a6db
Lucien Simpfendoerfer <lucien.simpfendoerfer@gmail.com>
2019-01-07 18:34:33 -0500

    initial commit

---
+++ extract_station_counts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Unzip two downloaded google drive folders, place in single folder
+tar -xvf ./data/NYC*.zip -C ./data
+tar -xvf ./data/Raw*.zip -C ./data
+mv ./data/'NYC Raw Bike Share Data'/* ./data/RawBikeData2
+rm -rf ./data/'NYC Raw Bike Share Data'
+
+# Place raw files in new directory, rename files so that they have a consistent name format. 
+mkdir ./data/station_counts
+for dir in ./data/RawBikeData2/*; 
+do 
+	echo ./data/station_counts/counts_${dir:20:4}_${dir:25:2}.csv
+	mv $dir/bikeshare_nyc_raw.csv ./data/station_counts/counts_${dir:20:4}_${dir:25:2}.csv
+done
+
+# Remove unneeded directory
+rm -rf ./data/RawBikeData2
